db25e8d156378d025ce4c5a9d3d8393d23379b69
Maíra Lourenço <mairalourenco.dev@gmail.com>
2025-09-02 00:30:21 -0300

    Reorganizando o código

--- exerciciosJS/funcoes.js
+++ exerciciosJS/funcoes.js
@@ -1,18 +1,18 @@
 const prompt = require('prompt-sync')();
 //Primeira função de teste
-/*function saudar(nome = "visitante") {
+function saudar(nome = "visitante") {
     console.log(`Olá, ${nome}! Seja bem-vindo(a)!`);
 }
 
-saudar("Maria");*/
+saudar("Maria");
 
 //Segunda função de teste
-/*function saudar(nome, idade, cidade) {
+function saudar(nome, idade, cidade) {
     console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`);
 }
 
 saudar();
-saudar("Maíra", 29, "Recife");*/
+saudar("Maíra", 29, "Recife");
 
 function formatarValor (valor) { //O valor nesse caso não é uma variável, é um parâmetro
     return `R$ ${valor.toFixed(2).replace(".", ",")}`; //toFixed também é uma função, porém já pronta, onde aqui estou determinando as casas decimais. E o replace vai realizar a substituição do ponto pela vírgula.
